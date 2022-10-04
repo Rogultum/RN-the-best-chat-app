89c1b7f021fa5f6f9e9ad318bc962ecd23e2b7dc
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-10-04 16:15:44 +0300

    feat: :card_file_box: add story document to firebase and add unique id to asyncstorage

--- src/screen/AuthNav-SignUp/SignUpScreen.js
+++ src/screen/AuthNav-SignUp/SignUpScreen.js
@@ -4,12 +4,12 @@
 import { useNavigation } from '@react-navigation/native';
 
 import React, { useState } from 'react';
-import { Alert, Pressable, SafeAreaView, ScrollView } from 'react-native';
-import { Text, useTheme } from 'react-native-paper';
+import { Alert, Pressable, SafeAreaView, View } from 'react-native';
+import { Text, TextInput, useTheme } from 'react-native-paper';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { GeoPoint, doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 
 import SignButton from '../../component/Button/SignButton';
 import Input from '../../component/Input';
@@ -18,14 +18,16 @@ import styles from './SignUpScreen.style';
 
 function SignUp() {
   const navigation = useNavigation();
+
   const { colors } = useTheme();
+
+  const [secureText, setSecureText] = useState(true);
 
   const [userMail, setUserMail] = useState(null);
   const [userPassword, setUserPassword] = useState(null);
   const [userPasswordCheck, setUserPasswordCheck] = useState(null);
   const [username, setUsername] = useState(null);
 
-  // TODO better alert
   const handleSignUp = async () => {
     if (userPassword !== userPasswordCheck) {
       return Alert.alert("Passwords don't match.");
@@ -39,37 +41,27 @@ function SignUp() {
       return Alert.alert('Missing Information');
     }
 
-    // TODO individual users
-    await AsyncStorage.setItem(
-      'user',
-      JSON.stringify({
-        email: userMail,
-        password: userPassword,
-        username,
-        photoURL: '',
-      })
-    );
-
     createUserWithEmailAndPassword(auth, userMail, userPassword).then(async (response) => {
       await setDoc(doc(db, 'user', response.user.uid), {
         id: response.user.uid,
         email: userMail,
         username,
         photoURL: '',
-        location: {
-          latitude: [],
-          longitude: [],
-        },
       });
-      await setDoc(doc(db, 'message', response.user.uid), {
+      await setDoc(doc(db, 'story', response.user.uid), {
         id: response.user.uid,
-        text: '',
-        location: {
-          latitude: '',
-          longitude: '',
-        },
-        geopoint: new GeoPoint(0, 0),
+        photoURL: [],
+        // geopoint: new GeoPoint(0, 0),
       });
+      await AsyncStorage.setItem(
+        response.user.uid,
+        JSON.stringify({
+          email: userMail,
+          password: userPassword,
+          username,
+          photoURL: '',
+        })
+      );
     });
 
     navigation.navigate('SignIn');
@@ -79,32 +71,49 @@ function SignUp() {
     navigation.navigate('SignIn');
   };
 
-  // TODO icons for text input
   return (
     <SafeAreaView style={[styles.container]}>
-      <ScrollView>
+      <View>
         <Input
           label="Type your e-mail"
           placeholder="example@example.com"
           onChangeText={setUserMail}
+          right={<TextInput.Icon icon="at" />}
         />
         <Input
           label="Type your password"
-          secureTextEntry
+          secureTextEntry={secureText}
           placeholder="*****"
           onChangeText={setUserPassword}
+          right={
+            <TextInput.Icon
+              onPress={() => setSecureText(!secureText)}
+              icon={secureText ? 'eye' : 'eye-outline'}
+            />
+          }
         />
         <Input
           label="Re-Type your password"
-          secureTextEntry
+          secureTextEntry={secureText}
           placeholder="*****"
           onChangeText={setUserPasswordCheck}
+          right={
+            <TextInput.Icon
+              onPress={() => setSecureText(!secureText)}
+              icon={secureText ? 'eye' : 'eye-outline'}
+            />
+          }
         />
-        <Input label="Type your username" placeholder="username" onChangeText={setUsername} />
+        <Input
+          label="Type your username"
+          placeholder="username"
+          onChangeText={setUsername}
+          right={<TextInput.Icon icon="account-outline" />}
+        />
         <Pressable onPress={navigateSignIn} style={styles.question_container}>
           <Text style={{ color: colors.secondary }}>Already have an account?</Text>
         </Pressable>
-      </ScrollView>
+      </View>
       <SignButton
         title="Sign Up"
         onPress={handleSignUp}
