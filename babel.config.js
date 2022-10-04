10f6159d6f03cfac741ee1f385ee48ced255eac4
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-10-04 16:09:24 +0300

    build: :wrench: add reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
+/* eslint-disable func-names */
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
       },
     },
   };
