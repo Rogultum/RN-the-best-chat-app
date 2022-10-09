00462c6f967b1dbb5f7eb99a609d5be3c37cfbc3
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-10-09 03:25:06 +0300

    :lipstick: add location modal styles

--- src/screen/ContactStack-Chat/ChatScreen.style.js
+++ src/screen/ContactStack-Chat/ChatScreen.style.js
@@ -16,4 +16,45 @@ export default StyleSheet.create({
   icon: {
     paddingTop: 9,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  close_button: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  selection_button: {
+    flexDirection: 'row',
+    marginBottom: 14,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    textAlign: 'center',
+  },
 });
