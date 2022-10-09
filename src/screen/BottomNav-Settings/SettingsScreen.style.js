64b8c9221fac8262bd24a5a746b54f2ed426d6f8
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-10-09 19:34:03 +0300

    :lipstick: add modal styles

--- src/screen/BottomNav-Settings/SettingsScreen.style.js
+++ src/screen/BottomNav-Settings/SettingsScreen.style.js
@@ -9,11 +9,53 @@ export default StyleSheet.create({
   save_button_container: {
     marginTop: 12,
   },
-  image: {},
+  list_item_container: {
+    flexDirection: 'row',
+    padding: 8,
+    margin: 8,
+  },
   theme_button_container: {
     marginTop: 57,
     marginRight: 55,
-    paddingTop: 500,
+    paddingTop: 350,
     position: 'absolute',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  close_button: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
